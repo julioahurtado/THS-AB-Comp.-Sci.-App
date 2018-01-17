4dc17a0b5528b4e5fa6f909493b64d7b6a48ca34
Julio Hurtado <juahurta@ucsc.edu>
2018-01-16 22:17:17 -0800

    Added Scheduke project files

---
+++ Scheduke/app/src/main/java/com/example/green1/SchedukeApp/Notifications.java
@@ -0,0 +1,34 @@
+package com.example.green1.SchedukeApp;
+
+import android.app.Notification;
+import android.support.v4.app.NotificationCompat;
+
+public class Notifications extends Notification
+{
+    private String message;
+    private String title;
+
+    public Notifications(String paramString1, String paramString2)
+    {
+        this.title = paramString1;
+        this.message = paramString2;
+    }
+
+    public static void notify(String paramString1, String paramString2)
+    {
+        MainActivity.setBuilder((android.support.v7.app.NotificationCompat.Builder) new NotificationCompat.Builder(MainActivity.MainContext));
+        MainActivity.getBuilder().setSmallIcon(2130837575);
+        MainActivity.getBuilder().setContentTitle(paramString1);
+        MainActivity.getBuilder().setContentText(paramString2);
+        MainActivity.getNotificationManager().notify(3967, MainActivity.getBuilder().build());
+    }
+
+    public void createNotification()
+    {
+        MainActivity.setBuilder((android.support.v7.app.NotificationCompat.Builder) new NotificationCompat.Builder(MainActivity.MainContext));
+        MainActivity.getBuilder().setSmallIcon(2130837575);
+        MainActivity.getBuilder().setContentTitle(this.title);
+        MainActivity.getBuilder().setContentText(this.message);
+        MainActivity.getNotificationManager().notify(3967, MainActivity.getBuilder().build());
+    }
+}
